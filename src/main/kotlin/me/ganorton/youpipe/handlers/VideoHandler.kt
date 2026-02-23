2d467257a2206356c596e2a44aa2596fd8b14aa1
Gregory Norton <gregory.norton@me.com>
2026-02-22 23:13:57 -0800

    Added initial video playback

--- src/main/kotlin/me/ganorton/youpipe/handlers/VideoHandler.kt
+++ src/main/kotlin/me/ganorton/youpipe/handlers/VideoHandler.kt
@@ -2,9 +2,27 @@ package me.ganorton.youpipe.handlers
 
 import io.vertx.core.Handler
 import io.vertx.ext.web.RoutingContext
+import org.schabi.newpipe.extractor.Extractor
+import org.schabi.newpipe.extractor.services.youtube.YoutubeService
+import org.schabi.newpipe.extractor.services.youtube.extractors.YoutubeStreamExtractor
+import org.schabi.newpipe.extractor.services.youtube.linkHandler.YoutubeStreamLinkHandlerFactory
 
 public class VideoHandler : Handler<RoutingContext> {
 	public override fun handle(ctx: RoutingContext) {
+		val id = ctx.queryParams().get("v") ?: ""
+		if (id.equals("")) {
+			ctx.next()
+			return@handle
+		}
+
+		val service = YoutubeService(0)
+
+		val linkHandler = YoutubeStreamLinkHandlerFactory.getInstance()
+		val streamExtractor = service.getStreamExtractor(linkHandler.getUrl(id))
+		streamExtractor.fetchPage()
+		streamExtractor.getDescription()
+		ctx.data<Extractor>().put("extractor", streamExtractor)
+
 		ctx.next()
 	}
 }
