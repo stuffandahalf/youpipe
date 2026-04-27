3cdecaf3be5d701211d0f4cc3843957e272da14e
Gregory Norton <gregory.norton@me.com>
2026-04-26 22:55:52 -0400

    Added early settings handling

--- src/main/kotlin/me/ganorton/youpipe/handlers/SettingsHandler.kt
+++ src/main/kotlin/me/ganorton/youpipe/handlers/SettingsHandler.kt
@@ -6,19 +6,28 @@ package me.ganorton.youpipe.handlers
 import io.vertx.ext.web.RoutingContext
 import me.ganorton.youpipe.PageHandler
 import me.ganorton.youpipe.managers.SettingsManager
-import me.ganorton.youpipe.managers.ApplicationSettings
 import me.ganorton.youpipe.managers.SubscriptionManager
 
 public class SettingsHandler(basePath: String, private val configPath: String) : PageHandler(basePath) {
-	private var lastLoaded: Int = 0
+	public override val supportHandlers: Map<String, (RoutingContext) -> Unit> = mapOf(
+		"update" to ::handleUpdate)
 
 	public override fun handle(ctx: RoutingContext) {
 		println("SettingsHandler::handle")
 
-		ctx.data<ApplicationSettings>().put("settings", SettingsManager.data)
+		ctx.data<SettingsManager.ApplicationSettings>().put("settings", SettingsManager.data)
 
 		ctx.data<List<SubscriptionManager.ImportStrategy>>().put("importStrategies", SubscriptionManager.ImportStrategy.entries)
 		ctx.data<List<SubscriptionManager.ExportSource>>().put("exportSources", SubscriptionManager.ExportSource.entries)
+	}
+
+	public fun handleUpdate(ctx: RoutingContext) {
+		val req = ctx.request()
+		val newSettings = SettingsManager.ApplicationSettings(
+			fastFetching = req.getFormAttribute("fastFetching") == "on")
+		SettingsManager.updateData(SettingsManager.ImportStrategy.OVERWRITE, newSettings)
+
+		ctx.redirect(this.basePath)
 	}
 }
 
